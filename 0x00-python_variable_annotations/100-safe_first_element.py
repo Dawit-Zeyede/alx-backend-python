a2936ed268b2d7fe6537277de98de430a813e4df
Dawit-Zeyede <devazeyede10@gmail.com>
2024-10-10 11:43:07 -0700

    sage

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,6 +6,8 @@ from typing import Sequence, Optional, Any
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    ''' Returs the first element if present or None
+    '''
     if lst:
         return lst[0]
     else:
