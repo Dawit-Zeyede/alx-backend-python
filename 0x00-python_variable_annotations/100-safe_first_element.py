77b4fd9e5598e64ff817b2f11e38b2c1693aad16
Dawit-Zeyede <devazeyede10@gmail.com>
2024-10-10 11:38:45 -0700

    100-sage

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Safe_first_element(lst).
+'''
+from typing import Sequence, Optional, Any
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    if lst:
+        return lst[0]
+    else:
+        return None
