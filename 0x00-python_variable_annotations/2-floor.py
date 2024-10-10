c854705c2e4ddcd1e99a2ce4d55b2d6ed46179f9
Dawit-Zeyede <devazeyede10@gmail.com>
2024-10-10 11:08:35 -0700

    2-floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Nearest integer.
+'''
+
+
+import math
+
+
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
+    '''
+    return math.floor(a)
